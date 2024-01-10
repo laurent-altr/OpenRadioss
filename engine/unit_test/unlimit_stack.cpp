5b61e4f30471f21ff6c16f4f2bdaa8fa37b37c66
laurent-altr <99423513+laurent-altr@users.noreply.github.com>
2024-01-10 12:21:17 +0100

    remove unused libraries from unit_test1

---
+++ engine/unit_test/unlimit_stack.cpp
@@ -0,0 +1,15 @@
+// set_stack.cpp
+
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+extern "C" {
+    void set_stack_unlimited() {
+        struct rlimit rl;
+        rl.rlim_cur = RLIM_INFINITY;
+        rl.rlim_max = RLIM_INFINITY;
+        setrlimit(RLIMIT_STACK, &rl);
+    }
+}
+
