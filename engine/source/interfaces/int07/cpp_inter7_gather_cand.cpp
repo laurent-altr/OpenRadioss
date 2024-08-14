097cbf2779efe47edcafed014bae8dc84aac1677
laurent-altr <99423513+laurent-altr@users.noreply.github.com>
2024-08-14 19:45:41 +0200

    update (not tested)

--- engine/source/interfaces/int07/cpp_inter7_gather_cand.cpp
+++ engine/source/interfaces/int07/cpp_inter7_gather_cand.cpp
@@ -824,9 +824,9 @@ extern "C"
                 //std::cout<<"bgapsmx="<<bgapsmx<<" bgapsmx_local="<<bgapsmx_local;
                 //std::cout<<" nbx="<<nbx<<" nby="<<nby<<" nbz="<<nbz<<" nbx_local="<<nbx_local<<" nby_local="<<nby_local<<" nbz_local="<<nbz_local<<std::endl;
             
-                nbx = nbx_local;
-                nby = nby_local;
-                nbz = nbz_local;
+            //    nbx = nbx_local;
+            //    nby = nby_local;
+            //    nbz = nbz_local;
             }
         }
 
